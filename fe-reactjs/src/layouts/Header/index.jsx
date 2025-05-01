f6f026c812df9c4646a99d1941eaf78e7988babc
quanghv20 <quanghv20@gmail.com>
2025-05-01 08:46:22 +0700

    Prevent users from pinch-zomming on mobile devices

--- fe-reactjs/src/layouts/Header/index.jsx
+++ fe-reactjs/src/layouts/Header/index.jsx
@@ -1,6 +1,6 @@
 /** React */
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 /** Components */
 import Dropdown from "components/Dropdown";
