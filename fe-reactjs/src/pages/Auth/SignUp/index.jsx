7ffdeadcf0f9fc75fbd4ae17b6c28e6fd401cb9a
quanghv20 <quanghv20@gmail.com>
2025-05-02 02:41:54 +0700

    Feature/UI-sign-up-page

--- fe-reactjs/src/pages/Auth/SignUp/index.jsx
+++ fe-reactjs/src/pages/Auth/SignUp/index.jsx
@@ -1,7 +1,109 @@
-import React from 'react'
+/** React */
+import React from "react";
+import { Link } from "react-router-dom";
+
+/** Components */
+import Logo from "components/Logo";
+import InputField from "components/InputField";
+
+/** Assets */
+import introImg from "assets/imgs/auth/intro.svg";
+import messageIcon from "assets/icons/message.svg";
+import lockIcon from "assets/icons/lock.svg";
+import googleIcon from "assets/icons/google.svg";
+
+/** Styles */
+import styles from "./styles.module.scss";
 
 export default function SignUpPage() {
   return (
-    <div>SignUpPage</div>
-  )
+    <div className={styles.auth}>
+      {/* Auth Intro */}
+      <div className={styles.auth__intro}>
+        <img src={introImg} alt="img" />
+        <p className={styles.auth__intro_text}>
+          The best of luxury brand values, high quality products, and innovative
+          services
+        </p>
+      </div>
+
+      {/* Auth Content */}
+      <div className={styles.auth__content}>
+        <div className={styles.auth__content_inner}>
+          <Logo />
+          <h1 className={styles.auth__heading}>Sign Up</h1>
+          <p className={styles.auth__desc}>
+            Let’s create your account and Shop like a pro and save money.
+          </p>
+          <form className={styles.auth__form}>
+            <InputField
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Email"
+              icon={messageIcon}
+            />
+            <InputField
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              icon={lockIcon}
+            />
+            <InputField
+              id="re-password"
+              name="re-password"
+              type="password"
+              placeholder="Confirm password"
+              icon={lockIcon}
+            />
+            <div className="d-flex justify-content-between mt-4">
+              <label
+                className={`d-flex align-items-center ${styles.checkbox}`}
+                style={{ cursor: "pointer" }}
+              >
+                <input type="checkbox" />
+                <span className="mt-1">Set as default card</span>
+              </label>
+              <span className={styles.hover_underline}>
+                <Link
+                  to="/sign-up"
+                  style={{
+                    color: "#00bcd4",
+                    cursor: "pointer",
+                  }}
+                >
+                  Recovery Password
+                </Link>
+              </span>
+            </div>
+            <div style={{ marginTop: 50 }}>
+              <button className="app-btn app-btn-primary w-100">Sign Up</button>
+              <button
+                className="app-btn app-btn-outline w-100"
+                style={{ marginTop: 20 }}
+              >
+                <img src={googleIcon} alt="" className="icon" />
+                <span>Sign in with Google</span>
+              </button>
+            </div>
+            <div style={{ marginTop: 100 }}>
+              <span style={{ marginRight: 8 }}>You have an account yet?</span>
+              <span className={styles.hover_underline}>
+                <Link
+                  to="/sign-in"
+                  style={{
+                    color: "#00bcd4",
+                    cursor: "pointer",
+                  }}
+                >
+                  Sign in
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
