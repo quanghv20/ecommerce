64d74992b7980281f737244c1787996b2fe72963
quanghv20 <quanghv20@gmail.com>
2025-05-02 20:10:58 +0700

    enhancement/ux - handle auto scroll top when route change

--- fe-reactjs/src/App.jsx
+++ fe-reactjs/src/App.jsx
@@ -1,10 +1,15 @@
+/** React - Router Dom */
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router";
+
+/** Components */
+import ScrollToTop from "components/ScrollToTop";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <ScrollToTop />
         <Router />
       </BrowserRouter>
     </>
